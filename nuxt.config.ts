e71fe70c59a778741ef27b0d933119ebd6726b6e
backiev <farkhat.bakiev@mail.ru>
2024-11-08 15:43:17 +0300

    added layout popup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts'],
-  css: ['~/assets/styles/index.scss']
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: ['@nuxt/fonts'],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                    additionalData: '@use "~/assets/styles/index.scss" as *;',
+                },
+            },
+        },
+    },
+    // css: ['~/assets/styles/index.scss'],
+    fonts: {
+        defaults: {
+            weights: [400],
+            styles: ['normal', 'italic'],
+            subsets: ['cyrillic-ext', 'cyrillic', 'greek-ext', 'greek', 'vietnamese', 'latin-ext', 'latin'],
+        },
+        families: [
+            {name: 'Unbounded', provider: 'google'},
+            {name: 'Onest', provider: 'google'},
+        ],
+    },
 })
