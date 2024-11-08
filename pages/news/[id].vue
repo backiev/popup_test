c66f982a1c72c0ccaeab793c9656ded0b791a8e1
backiev <farkhat.bakiev@mail.ru>
2024-11-08 13:07:02 +0300

    added base sctructure

---
+++ pages/news/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>123123</div>
+</template>
+
+<script lang="ts" setup>
+    definePageMeta({
+        layout: 'popup',
+    })
+    const route = useRoute()
+
+    // When accessing /posts/1, route.params.id will be 1
+    console.log(route.params.id)
+</script>
+
+<style></style>
