a231f57009f116c215f577ed8302e11afcd4427b
backiev <farkhat.bakiev@mail.ru>
2024-11-10 16:58:24 +0300

    added gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: ['@nuxt/fonts'],
-
+    app: {
+        baseURL: '/popup_test/',
+    },
     vite: {
         css: {
             preprocessorOptions: {
